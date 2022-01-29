a9b26d1df2d2e8d0503f1530e9590686665be3c3
Anuj Bhalla <41057359+AnujBhalla@users.noreply.github.com>
2022-01-29 15:03:10 +0000

    Delete Q19_supp.py

--- Q11-20/Q19_supp.py
+++
@@ -1,7 +0,0 @@
-Normal_Year = [31,28,31,30,31,30,31,31,30,31,30,31]
-Cumulative_Normal_Year = [sum(Normal_Year[:i]) for i in range(1, len(Normal_Year))]
-Leap_Year = Normal_Year 
-Leap_Year[1]=29
-Cumulative_Leap_Year = [sum(Leap_Year[:i]) for i in range(1, len(Leap_Year))]
-
-print(Cumulative_Normal_Year)
